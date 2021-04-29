b9d9cc2a1f1149bfd3fc5ca6647849c0bfb718c9
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2021-04-29 12:19:31 -0300

    Cria classe para renderizar uma spritesheet.

---
+++ app/sprite/sheet.rb
@@ -0,0 +1,21 @@
+module Sprite
+  class Sheet
+    def self.render(opts)
+      height = opts[:h]
+      width = opts[:w]
+      sprite_index = opts[:tick].frame_index(count: 4, hold_for: 1, repeat: true)
+
+      {
+        x: opts[:x],
+        y: opts[:y],
+        w: width,
+        h: width,
+        path: opts[:path],
+        source_x: width * sprite_index,
+        source_y: 0,
+        source_w: width,
+        source_h: height
+      }
+    end
+  end
+end
